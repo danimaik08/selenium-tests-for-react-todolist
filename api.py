9a4d86e9f7cf25cc171ac5cb220f7e27c9d9fe1d
Daniil Maikovskii <daniil.maykovskiy@mail.ru>
2024-09-29 01:05:59 +0300

    BrowserAPI is singleton

--- api.py
+++ api.py
@@ -5,6 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from settings import TARGET
 
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+@singleton
 class BrowserAPI:
     def __init__(self):
         self.browser = webdriver.Chrome()
